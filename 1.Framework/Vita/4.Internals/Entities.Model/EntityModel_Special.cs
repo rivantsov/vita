1b6543e170aebf8b9928920c9fe85712ee786457
Roman Ivantsov <rivantsov@gmail.com>
2019-07-11 23:48:16 -0700

    entity views added

---
+++ 1.Framework/Vita/4.Internals/Entities.Model/EntityModel_Special.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vita.Data.Linq.Translation.Expressions;
+
+namespace Vita.Entities.Model.Special {
+
+  // Note: for now, view is join of entities only, not views
+  public class EntityView {
+    public string Name; 
+    public List<JoinPart> Joins = new List<JoinPart>();
+    public List<ViewMember> GroupBy = new List<ViewMember>();
+
+    public EntityView(EntityInfo entity) {
+      var jEnt = new JoinPart() { Entity = entity };
+      Joins.Add(jEnt);
+      Name = "V_" + entity.Name;
+    }
+  }
+
+  public class ViewMember {
+    public string Name; 
+    public EntityMemberInfo SourceMember;
+    public JoinPart JoinPart;
+    public AggregateType Aggregate;
+    public string AggregateExpression;
+    public string Expr;
+  }
+
+  public class JoinPart {
+    public EntityInfo Entity;
+    public TableJoinType JoinType;
+    public EntityMemberInfo JoinMember;
+    public string Alias;
+  }
+
+
+
+
+}

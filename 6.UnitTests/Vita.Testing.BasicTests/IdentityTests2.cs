5119dccf688dd7a50da6c753b04adfbc30f8d9c3
Roman Ivantsov <rivantsov@gmail.com>
2019-07-27 08:49:17 -0700

    added IdentityTests2

---
+++ 6.UnitTests/Vita.Testing.BasicTests/IdentityTests2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Vita.Entities;
+using Vita.Entities.Runtime;
+
+namespace Vita.Testing.BasicTests.IdentityTests2 {
+
+  // Special identity tests
+  // Special case - composite PK, with FK to parent (which is identity) and another identity
+
+  [Entity]
+  public interface IUser {
+    [PrimaryKey, Identity]
+    int Id { get; set; }
+    [Size(30)]
+    string Name { get; set; }
+  }
+
+  [Entity, PrimaryKey("User,OrderId")]
+  public interface IUserPost {
+    IUser User { get; set; }
+    [Identity]
+    int OrderId { get; set; }
+    [Size(100)]
+    string Text { get; set; }
+  }
+
+  // We skip defining custom entity module and use base EntityModule class
+  public class IdentityTests2EntityApp : EntityApp {
+    public IdentityTests2EntityApp() {
+      var area = AddArea("ident2");
+      var mainModule = new EntityModule(area, "MainModule");
+      mainModule.RegisterEntities(typeof(IUser), typeof(IUserPost));
+    }
+  }//class
+
+
+  [TestClass]
+  public class IdentityColumnsTest {
+    EntityApp _app;
+
+    [TestCleanup]
+    public void TestCleanup() {
+      if (_app != null)
+        _app.Flush();
+    }
+
+    [TestMethod]
+    public void TestIdentityWithCompositePK() {
+      // test with and without batch mode
+      RunTest(false);
+      RunTest(true); 
+    }
+
+    public void RunTest(bool batchMode) {
+      _app = new IdentityTests2EntityApp();
+      Startup.ActivateApp(_app);
+      //if(Startup.ServerType == Data.Driver.DbServerType.SQLite)
+      DeleteAllData();
+      // We create session this way to set the batch mode flag
+      var ctx = new OperationContext(_app); 
+      IEntitySession session = new EntitySession(ctx, 
+        options: batchMode ? EntitySessionOptions.None : EntitySessionOptions.DisableBatchMode);
+
+      var john = session.NewUser("john");
+      var post1 = john.NewPost("john post 1");
+      var post2 = john.NewPost("john post 2");
+      var post3 = john.NewPost("john post 3");
+      var ben = session.NewUser("ben");
+      var post1b = john.NewPost("ben post 1");
+      var post2b = john.NewPost("ben post 2");
+      var post3b = john.NewPost("ben post 3");
+      session.SaveChanges();
+      //Check that Identity values immediately changed to actual positive values loaded from database
+      Assert.IsTrue(post1.OrderId > 0, "Invalid OrderID - expected positive value.");
+      Assert.IsTrue(post2.OrderId > 0, "Invalid OrderID - expected positive value.");
+      Assert.IsTrue(post3.OrderId > 0, "Invalid OrderID - expected positive value.");
+
+      //Start new session, load all and check that IDs and relationships are correct
+      session = _app.OpenSession();
+      var posts = session.EntitySet<IUserPost>().ToList();
+      foreach (var post in posts)
+        Assert.IsTrue(post.OrderId > 0);
+
+
+    }//method
+
+    // For SQLite objects are not dropped (FK problems), so we need to instead delete all data
+    // to make sure we start with empty tables
+    private void DeleteAllData() {
+      var session = _app.OpenSession();
+      session.ExecuteDelete<IUserPost>(session.EntitySet<IUserPost>());
+      session.ExecuteDelete<IUser>(session.EntitySet<IUser>());
+    }
+
+  }
+
+  static class Id2Extensions {
+    public static IUser NewUser(this IEntitySession session, string name) {
+      var user = session.NewEntity<IUser>();
+      user.Name = name;
+      return user;
+    }
+    public static IUserPost NewPost(this IUser user, string text) {
+      var session = EntityHelper.GetSession(user);
+      var post = session.NewEntity<IUserPost>();
+      post.User = user;
+      post.Text = text;
+      return post; 
+    }
+  }//IdExtensions class
+}

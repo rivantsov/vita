d8f2b42283205093d9806447711eda1223de68ee
rivantsov@gmail.com <rivantsov@gmail.com>
2021-06-03 00:45:09 -0700

    minor edits

---
+++ src/6.UnitTests/Vita.Testing.GraphQLTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vita.Testing.GraphQLTests {
+  public class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("The test project cannot be run as Console app. Execute tests in Test Explorer.");
+      Console.WriteLine("Press any key...");
+      Console.ReadKey(); 
+    }
+  }
+}

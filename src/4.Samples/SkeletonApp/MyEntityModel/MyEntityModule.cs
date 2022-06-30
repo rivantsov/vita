daf6c523c4d18ae68dfb2dd19445ad4646c198d9
Roman Ivantsov <roman.ivantsov@microsoft.com>
2022-06-30 13:17:59 -0700

    upgraded package versions

--- src/4.Samples/SkeletonApp/MyEntityModel/MyEntityModule.cs
+++ src/4.Samples/SkeletonApp/MyEntityModel/MyEntityModule.cs
@@ -7,7 +7,7 @@ namespace MyEntityModel
   {
     public static readonly Version CurrentVersion = new Version("1.0.0.0");
 
-    public MyEntityModule(EntityArea area) : base(area, nameof(MyEntityModule))
+    public MyEntityModule(EntityArea area) : base(area, nameof(MyEntityModule), "My entity module", CurrentVersion)
     {
       // Register all entity types
       RegisterEntities(typeof(ICustomer));

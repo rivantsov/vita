edaa29be634d509b80c8376b827b734a473b29d9
rivantsov2 <rivantsov@gmail.com>
2022-07-05 07:16:18 -0700

    wip

--- src/1.Framework/Vita/4.Internals/Entities.Model/Construction/EntityModelBuilder_KeysNew.cs
+++ src/1.Framework/Vita/4.Internals/Entities.Model/Construction/EntityModelBuilder_KeysNew.cs
@@ -9,5 +9,16 @@ namespace Vita.Entities.Model.Construction {
   // key expansion methods
   public partial class EntityModelBuilder {
 
+    private void ExpandEntityKeyMembersNew() {
+      var allKeys = Model.Entities.SelectMany(e => e.Keys).ToList(); 
+
+
+      CheckErrors();
+    }
+
+    private bool TryExpandKey(EntityKeyInfo key) {
+      
+    }
+
   }
 }
